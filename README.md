ce0da6d8ebce0907d45b265061ea3cde321d6d7a
pfansiW <fancyfreelance76@gmail.com>
2021-11-30 03:39:11 +0000

    updated index.html, style.css, script.js file

--- README.md
+++ README.md
@@ -7,3 +7,35 @@ When the user enters his desired city into the app and searches ,
 he will be presented with a detailed weather result.
 one part of the result will be the current detailed weather condition and the other section of the result will be information about the next 5 days' weather forecast .
 The past searched cities by the users will be saved on one section of the dashboard. This will improve the user experience of the app by allowing the user to just click on the previously searched city to get the same weather information as previously using the search method.
+
+## Git Repository
+
+I created a git repository folder that will host the finish website. The Repository will be found [here](https://github.com/pfansi/weather-display-app)
+
+## What have you done
+
+- Created a readme.md file to describe the different building phases of the application.
+
+- created an index.html, a css file, a javascript file.
+
+- created the application layout using bootstrap.
+
+- created the css file to style to add additional style to the bootstrap layout.
+
+- created the function to check if there is data in the local storage.
+
+- created a function that makes a call to the API and returns the value data of the current city name entered by the user.
+
+- created a function that makes a call to the API and returns the value of the weather forecast for the next five days.
+
+## Screenshots
+
+- screenshot of the final application. ![screenshot](./images/weather_dashboard.jpg)
+
+## Link to the deploy application
+
+https://pfansi.github.io/weather-display-app/
+
+## link to the git hub repository
+
+https://github.com/pfansi/weather-display-app
---
+++ script.js
@@ -0,0 +1,107 @@
+//let declares these variables 
+var searchButton = $(".searchButton");
+
+var apiKey = "b8ecb570e32c2e5042581abd004b71bb";
+
+// we loop into the storage to check if there is any saved data 
+for (var i = 0; i < localStorage.length; i++) {
+
+    var saveCity = localStorage.getItem(i);
+    
+    var cityEnter = $(".list-group").addClass("list-group-item");
+
+    cityEnter.append("<li>" + saveCity + "</li>");
+}
+
+var keyCount = 0;
+
+// when the search button is clicked , this function is called 
+searchButton.click(function () {
+
+    var searchInput = $(".searchInput").val();
+
+    // let create variable for the API data 
+    var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    // Variable for 5 day forecast working
+    var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+
+
+    if (searchInput == "") {
+        console.log(searchInput);
+    } else {
+        $.ajax({
+            url: urlWeather,
+            method: "GET"
+        }).then(function (response) {
+
+            //This variable will append the list of searched city
+            var cityEnter = $(".list-group").addClass("list-group-item");
+            cityEnter.append("<li>" + response.name + "</li>");
+            // // Local storage
+            // var local = localStorage.setItem(keyCount, response.name);
+            // keyCount = keyCount + 1;
+
+            // This create a div and append the current weather deta from the API
+            var currentWeather = $(".currentWeather").append("<div>").addClass("card-body");
+            currentWeather.empty();
+            var weatherData = currentWeather.append("<p>");
+
+            // append the time data 
+            var timeUTC = new Date(response.dt * 1000);
+            weatherData.append("<p>" + response.name + " " + timeUTC.toLocaleDateString("en-US") + "</p>");
+
+            // append the weather image
+            weatherData.append("<div>" + `<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">` + "</div>");
+
+            //append the temperature data
+            weatherData.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+
+            // append the Humidity data
+            weatherData.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+
+            // append the Wind Speed data
+            weatherData.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+
+            // let declare the variable url for the uvi data
+            var urlUvi = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+            // this will query the API and return the uvi data
+            $.ajax({
+                url: urlUvi,
+                method: "GET"
+            }).then(function (response) {
+                
+                // let add a class to the uvi data for the style and append its value
+                weatherData.append("<div class=UV>" + "UV Index: " + response.value + "</div>");
+
+
+            });
+
+        });
+
+        // another request is made to the API to return the 5 days forecast data
+        $.ajax({
+            url: urlForecast,
+            method: "GET"
+        }).then(function (response) {
+            // let declare the variable array for the five days
+            var fiveDay = [0, 8, 16, 24, 32];
+            var fivedayData = $(".fivedayForecast").addClass("card-text");
+            fivedayData.empty();
+
+            //we loop in to the variable fiveDay 
+            fiveDay.forEach(function (i) {
+                var fivedayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                fivedayTimeUTC1 = fivedayTimeUTC1.toLocaleDateString("en-US");
+
+                fivedayData.append("<div class=fivedayStyle>" + "<p>" + fivedayTimeUTC1 + "</p>" 
+                + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` 
+                + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+
+
+            })
+
+        });
+    }
+}); 
+
